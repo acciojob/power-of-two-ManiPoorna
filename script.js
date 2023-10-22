e940bd6d72fc6d2d688645854a1427d95352a8e9
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-22 11:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	let num = 2;
+	while (num <= number) {
+		if (num === number) {
+			return true
+		}
+		if (num > number) {
+			break
+		}
+		num *= 2
+	}
+	return false
 };
 
-const num = parseInt(prompt("Enter a number"));
+// const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
